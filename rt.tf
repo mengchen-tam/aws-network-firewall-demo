f881ac0db05adb52c03cb6a3e5350726e733bdd4
mengchen <mengchs@nwcdcloud.cn>
2025-01-10 09:37:41 +0000

    update hardcode AZ info to dynamic var

--- rt.tf
+++ rt.tf
@@ -19,22 +19,22 @@ resource "aws_default_route_table" "default" {
 
 # Route table for TGW attachment subnet in AZ 1a
 # Routes all traffic to Network Firewall endpoint
-resource "aws_route_table" "tgw_attach_subnet_cn_northwest_1a" {
+resource "aws_route_table" "tgw_attach_subnet_az1" {
   vpc_id = aws_vpc.vpc.id
   route {
     cidr_block      = "0.0.0.0/0"
     vpc_endpoint_id = [for state in aws_networkfirewall_firewall.inspection.firewall_status[0].sync_states : state.attachment[0].endpoint_id if state.availability_zone == aws_subnet.tgw_attach_subnet[0].availability_zone][0]
   }
   tags = {
-    "Name" = "tgw_attach_subnet_cn_northwest_1a_rtb"
+    "Name" = "tgw_attach_subnet_${data.aws_availability_zones.available.names[0]}_rtb"
   }
 }
 
 
 # Associate TGW attachment subnet in AZ 1a with its route table
-resource "aws_route_table_association" "tgw_attach_subnet_cn_northwest_1a_rt" {
+resource "aws_route_table_association" "tgw_attach_subnet_az1_rt" {
   subnet_id      = aws_subnet.tgw_attach_subnet.*.id[0]
-  route_table_id = aws_route_table.tgw_attach_subnet_cn_northwest_1a.id
+  route_table_id = aws_route_table.tgw_attach_subnet_az1.id
   depends_on = [
     aws_subnet.tgw_attach_subnet
   ]
@@ -42,21 +42,21 @@ resource "aws_route_table_association" "tgw_attach_subnet_cn_northwest_1a_rt" {
 
 # Route table for TGW attachment subnet in AZ 1b
 # Routes all traffic to Network Firewall endpoint
-resource "aws_route_table" "tgw_attach_subnet_cn_northwest_1b" {
+resource "aws_route_table" "tgw_attach_subnet_az2" {
   vpc_id = aws_vpc.vpc.id
   route {
     cidr_block      = "0.0.0.0/0"
     vpc_endpoint_id = [for state in aws_networkfirewall_firewall.inspection.firewall_status[0].sync_states : state.attachment[0].endpoint_id if state.availability_zone == aws_subnet.tgw_attach_subnet[1].availability_zone][0]
   }
   tags = {
-    "Name" = "tgw_attach_subnet_cn_northwest_1b_rtb"
+    "Name" = "tgw_attach_subnet_${data.aws_availability_zones.available.names[1]}_rtb"
   }
 }
 
 # Associate TGW attachment subnet in AZ 1b with its route table
-resource "aws_route_table_association" "tgw_attach_subnet_cn_northwest_1b_rt" {
+resource "aws_route_table_association" "tgw_attach_subnet_az2_rt" {
   subnet_id      = aws_subnet.tgw_attach_subnet.*.id[1]
-  route_table_id = aws_route_table.tgw_attach_subnet_cn_northwest_1b.id
+  route_table_id = aws_route_table.tgw_attach_subnet_az2.id
   depends_on = [
     aws_subnet.tgw_attach_subnet
   ]
@@ -65,9 +65,7 @@ resource "aws_route_table_association" "tgw_attach_subnet_cn_northwest_1b_rt" {
 
 #### Fireawll  Subnet Routes - Inspection VPC ####
 
-
-
-resource "aws_route_table" "firewall_subnet_cn_northwest_1a" {
+resource "aws_route_table" "firewall_subnet_az1" {
   vpc_id = aws_vpc.vpc.id
   route {
     cidr_block         = var.parent_cidr_block
@@ -78,22 +76,20 @@ resource "aws_route_table" "firewall_subnet_cn_northwest_1a" {
     nat_gateway_id = aws_nat_gateway.nat_gateway.*.id[0]
   }
   tags = {
-    "Name" = "firewall_subnet_cn_northwest_1a_rtb"
+    "Name" = "firewall_subnet_${data.aws_availability_zones.available.names[0]}_rtb"
   }
 }
 
 
-resource "aws_route_table_association" "firewall_subnet_rt_cn_northwest_1a" {
+resource "aws_route_table_association" "firewall_subnet_rt_az1" {
   subnet_id      = aws_subnet.firewall_subnet.*.id[0]
-  route_table_id = aws_route_table.firewall_subnet_cn_northwest_1a.id
+  route_table_id = aws_route_table.firewall_subnet_az1.id
   depends_on = [
     aws_subnet.firewall_subnet
   ]
 }
 
-
-
-resource "aws_route_table" "firewall_subnet_cn_northwest_1b" {
+resource "aws_route_table" "firewall_subnet_az2" {
   vpc_id = aws_vpc.vpc.id
   route {
     cidr_block         = var.parent_cidr_block
@@ -104,13 +100,13 @@ resource "aws_route_table" "firewall_subnet_cn_northwest_1b" {
     nat_gateway_id = aws_nat_gateway.nat_gateway.*.id[1]
   }
   tags = {
-    "Name" = "firewall_subnet_cn_northwest_1b_rtb"
+    "Name" = "firewall_subnet_${data.aws_availability_zones.available.names[1]}_rtb"
   }
 }
 
-resource "aws_route_table_association" "firewall_subnet_rt_cn_northwest_1b" {
+resource "aws_route_table_association" "firewall_subnet_rt_az2" {
   subnet_id      = aws_subnet.firewall_subnet.*.id[1]
-  route_table_id = aws_route_table.firewall_subnet_cn_northwest_1b.id
+  route_table_id = aws_route_table.firewall_subnet_az2.id
   depends_on = [
     aws_subnet.firewall_subnet
   ]
@@ -119,7 +115,7 @@ resource "aws_route_table_association" "firewall_subnet_rt_cn_northwest_1b" {
 
 #### NAT GATEWAY  Subnet Routes - Inspection VPC ####
 
-resource "aws_route_table" "public_subnet_cn_northwest_1a" {
+resource "aws_route_table" "public_subnet_az1" {
   vpc_id = aws_vpc.vpc.id
 
   route {
@@ -133,20 +129,20 @@ resource "aws_route_table" "public_subnet_cn_northwest_1a" {
   }
 
   tags = {
-    "Name" = "public_subnet_cn_northwest_1a_rtb"
+    "Name" = "public_subnet_${data.aws_availability_zones.available.names[0]}_rtb"
   }
 }
 
 
-resource "aws_route_table_association" "public_subnet_cn_northwest_1a_rt" {
+resource "aws_route_table_association" "public_subnet_az1_rt" {
   subnet_id      = aws_subnet.public_subnet[0].id
-  route_table_id = aws_route_table.public_subnet_cn_northwest_1a.id
+  route_table_id = aws_route_table.public_subnet_az1.id
   depends_on = [
     aws_subnet.public_subnet
   ]
 }
 
-resource "aws_route_table" "public_subnet_cn_northwest_1b" {
+resource "aws_route_table" "public_subnet_az2" {
   vpc_id = aws_vpc.vpc.id
 
   route {
@@ -160,13 +156,13 @@ resource "aws_route_table" "public_subnet_cn_northwest_1b" {
   }
 
   tags = {
-    "Name" = "public_subnet_cn_northwest_1b_rtb"
+    "Name" = "public_subnet_${data.aws_availability_zones.available.names[1]}_rtb"
   }
 }
 
-resource "aws_route_table_association" "public_subnet_cn_northwest_1b_rt" {
+resource "aws_route_table_association" "public_subnet_az2_rt" {
   subnet_id      = aws_subnet.public_subnet[1].id
-  route_table_id = aws_route_table.public_subnet_cn_northwest_1b.id
+  route_table_id = aws_route_table.public_subnet_az2.id
   depends_on = [
     aws_subnet.public_subnet
   ]
