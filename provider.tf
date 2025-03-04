6e6104f40b0bd4230a3ea179fc2383480a8a713f
mengchen <mengchs@nwcdcloud.cn>
2025-03-04 02:37:04 +0000

    update AMI filter

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ data "aws_ami" "amazon_linux_2023" {
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["al2023-ami-2023*-kernel-6.1-x86_64"]
   }
 
   filter {
